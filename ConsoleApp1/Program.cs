6ae24ffaf0eec98d51d151cff27b5999897e1e07
Vyaliy <Vyaltsevn@mail.ru>
2023-01-01 23:25:40 +0300

    Module4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,16 +4,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-
-        string MyName = "Jane";
-        byte MyAge = 27;
-        bool HaveIApet = true;
-        double MyShoeSize = 37.5;
-
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("MyAge " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIApet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
+        double result = 5 / 2 * 3;
 
         Console.ReadKey();
 
